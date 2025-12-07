7618f5ecac70f6f3208288f0d58b008ca99b7bed
gabrielagpuig <gabi.g.puig@gmail.com>
2025-12-07 19:40:19 +0100

    Remove arquivos ignorados

---
+++ lingotlow-website/app/documents/page.tsx
@@ -0,0 +1,8 @@
+export default function DocumentsPage() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Documentos</h1>
+      <p>Aqui você verá os documentos enviados e seus status.</p>
+    </div>
+  );
+}
